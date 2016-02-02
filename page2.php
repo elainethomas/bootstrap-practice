cd7cbfe7c129b9546a0011412e0de759e845bb71
Elaine Thomas <enajera2@cnm.edu>
2016-02-02 11:13:41 -0700

    adding more code to page 2

--- page2.php
+++ page2.php
@@ -32,10 +32,95 @@
 	<body class="sfooter">
 		<div class="sfooter-content">
 			<header>
-				header here
+				<nav class="navbar navbar-default"> <!--use navbar-inverse for darker bg-->
+					<div class="container-fluid">
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#top-nav" aria-expanded="false">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+							<!--<a class="navbar-brand" href="#">Brand</a> remove this line, add <img src="blah.jpg" alt="blah">-->
+							<img src="logo.png" alt="logo">
+						</div>
+
+						<!-- Collect the nav links, forms, and other content for toggling -->
+						<div class="collapse navbar-collapse" id="top-nav">
+							<ul class="nav navbar-nav">
+								<li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+								<li><a href="#">Link</a></li>
+								<li class="dropdown">
+									<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+									<ul class="dropdown-menu">
+										<li><a href="#">Action</a></li>
+										<li><a href="#">Another action</a></li>
+										<li><a href="#">Something else here</a></li>
+										<li role="separator" class="divider"></li>
+										<li><a href="#">Separated link</a></li>
+										<li role="separator" class="divider"></li>
+										<li><a href="#">One more separated link</a></li>
+									</ul>
+								</li>
+							</ul>
+							<form class="navbar-form navbar-left" role="search">
+								<div class="form-group">
+									<input type="text" class="form-control" placeholder="Search">
+								</div>
+								<button type="submit" class="btn btn-default">Submit</button>
+							</form>
+							<ul class="nav navbar-nav navbar-right">
+								<li><a href="#">Link</a></li>
+								<li class="dropdown">
+									<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+									<ul class="dropdown-menu">
+										<li><a href="#">Action</a></li>
+										<li><a href="#">Another action</a></li>
+										<li><a href="#">Something else here</a></li>
+										<li role="separator" class="divider"></li>
+										<li><a href="#">Separated link</a></li>
+									</ul>
+								</li>
+							</ul>
+						</div><!-- /.navbar-collapse -->
+					</div><!-- /.container-fluid -->
+				</nav>
 			</header>
 			<main>
-				main area
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+							<div class="content-box">
+							meka leka hi
+							</div>
+					</div>
+				</div>
+							<div class="row">
+						<div class="col-md-6">
+							words
+						</div>
+						<div class="col-md-6">
+							here
+						</div>
+
+					</div>
+					<div class="row">
+						<div class="col-md-3">
+							last row 1
+						</div>
+						<div class="col-md-3">
+							last row 2
+						</div>
+						<div class="col-md-3">
+							last row 3
+						</div>
+						<div class="col-md-3">
+							last row 4
+						</div>
+
+					</div>
+				</div>
 			</main>
 		</div>
 		<footer>
